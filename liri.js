cff643acb0d345e0d6171e2ecdfdfe0840f3729b
Eri <eriinwanderland@calvisitor-10-105-148-102.calvisitor.1918.berkeley.edu>
2017-07-12 17:14:59 -0700

    fixing up spotify and movie functions and adding the log.txt file and do what it says function

--- liri.js
+++ liri.js
@@ -1,36 +1,72 @@
-//Mr. Nobody data
-//ace of base
-//spotify client ID and client secret
-//spotify search function??
+//log.txt
+//spotify this ace of base
 
-var userCommand = process.argv[3];
+var userCommand = process.argv[2];
 var nodeArgs = process.argv;
 
 var Spotify = require('node-spotify-api');
 var Twitter = require('twitter');
 var request = require('request');
+var fs = require('fs');
+
 
 var spotify = new Spotify({
   id: '9461e9f48abe4e4fb804c2af0eba711b',
   secret: '3c771f191c804a4fb1300893c942b992'
 });
 
+function doWhatItSays(){
+	fs.readFile('random.txt', "utf8", function(err, data) {
+		if (err) {
+	    	console.log(err);
+	  	}
+	  	else {
+	    	var dataArr = data.split(', ');
+	    	var songName = dataArr[1].slice(1, -1);
+	  	}
+	spotify.search({ type: 'track', query: songName, limit: 1 }, function(err, data) {
+	  	if (err) {
+	    	return console.log('Error occurred: ' + err);
+		}
+		else {
+		  	// console.log(data);
+		  	var songs = data.tracks.items;
+		  	// console.log(songs);
+		  	songs.forEach(function(song) {
+		  		console.log(song.preview_url);
+		  		console.log(song.album.artists[0].name);
+		  		console.log(song.name);
+		  		console.log(song.album.name);
+		  		console.log('------------------------------');
+		 	});
+		}
+	});
+	});
+}
+
 function spotifyThis(){
-	var songName = process.argv.slice(3).join(" ");
-	spotify.search({ type: 'track', query: songName }, function(err, data) {
+	var songName = '';
+		if (process.argv[3] === undefined){
+			songName = 'The Sign';
+		} 
+		else {
+			songName = process.argv.slice(3).join(" ");
+		}
+
+	spotify.search({ type: 'track', query: songName, limit: 1 }, function(err, data) {
 	  if (err) {
 	    return console.log('Error occurred: ' + err);
 	  }
 	  else {
 	  	// console.log(data);
 	  	var songs = data.tracks.items;
-	  	console.log(songs);
+	  	// console.log(songs);
 	  	songs.forEach(function(song) {
-	  		// console.log(song.preview_url);
-	  		// console.log('--------------------------');
-	  		console.log(song.album.artists);
-	  		// console.log(song.name);
-	  		// console.log(song.album);
+	  		console.log(song.preview_url);
+	  		console.log(song.album.artists[0].name);
+	  		console.log(song.name);
+	  		console.log(song.album.name);
+	  		console.log('------------------------------');
 	  	});
 	  }
 	});
@@ -54,25 +90,19 @@ function myTweets(){
 
 function movieThis(){
 	var movieName = '';
-	for (var i = 3; i < nodeArgs.length; i++){
-		if (userCommand === undefined){
-			movieName === 'Mr. Nobody';
-		}
-		else if(i > 3 && i < nodeArgs.length){
-			movieName += '+' + nodeArgs[i];
-		}
+		if (process.argv[3] === undefined){
+			movieName = 'Mr. Nobody';
+			console.log('Watch Mr. Nobody!');
+		} 
 		else {
-			movieName = nodeArgs[i];
+			movieName = process.argv.slice(3).join(" ");
 		}
-	};
-	console.log(movieName);
 
 	var queryURL = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
-		console.log(queryURL);
 
 	request(queryURL, function(error, response, body){
 		if (!error && response.statusCode === 200 && userCommand !== " "){
-			// console.log(body);
+			
 			console.log("Title: " + JSON.parse(body).Title);
 			console.log("Year: " + JSON.parse(body).Year);
 			console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
@@ -85,18 +115,32 @@ function movieThis(){
 	});
 }
 
+function addToTextFile(){
+	fs.appendFile('log.txt', ', ' +  function(err) {
+	if (err) {
+	    console.log(err);
+	  	}
+	  	else {
 
-// switch (userCommand){
-// 	case 'movie-this':
+	  	}
+	});
+}
+
+
+switch (userCommand){
+	case 'movie-this':
 		movieThis();
-// 		break;
-// 	case 'my-tweets': 
-// 		myTweets();
-// 		break;
-// 	case 'spotify-this':
-// 		spotifyThis();
-// 		break;
-// }
+		break;
+	case 'my-tweets': 
+		myTweets();
+		break;
+	case 'spotify-this':
+		spotifyThis();
+		break;
+	case 'do-what-it-says':
+		doWhatItSays();
+		break;
+}
 
 
 
